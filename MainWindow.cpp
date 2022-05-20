70355d0e5e493665deaeb072d0c5738f58446488
mr.Escow <escowxbox@gmail.com>
2022-05-20 17:47:37 +0300

    Add Main wgt and layout

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_MainWindow.h"
 
 #include "Wgt_RootQuadraticEquationSecondDegree.h"
+#include "Wgt_CalculationSideTriangle_onTwoSides_and_AngleBetween.h"
+
+#include <QLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,8 +12,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    auto FirstTask = new Wgt_RootQuadraticEquationSecondDegree(this);
+    auto MAIN_WGT = new QWidget(this);
+    MAIN_WGT->resize(this->size());
 
+    auto FirstTask = new Wgt_RootQuadraticEquationSecondDegree(MAIN_WGT);
+    auto SecondTask = new Wgt_CalculationSideTriangle_onTwoSides_and_AngleBetween(MAIN_WGT);
+    //auto TheeTask = new Wgt_Two_TextEditors_with_html(this);
+
+    auto Vertical_Layout = new QVBoxLayout(MAIN_WGT);
+    Vertical_Layout->addWidget(FirstTask);
+    Vertical_Layout->addWidget(SecondTask);
+    //Vertical_Layout->addWidget(TheeTask);
 
 }
 
