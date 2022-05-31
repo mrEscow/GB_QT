3593c16fd73b9c7181f5385a91d1f335385490d8
mr.Escow <escowxbox@gmail.com>
2022-06-01 01:42:11 +0300

    Models

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,36 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
-
+/*
+    Создать список с названиями языков программирования (С++, Python, Java, C#, PHP, JavaScript).
+    Для каждого элемента списка рядом отобразить иконку с логотипом языка программирования.
+    Предусмотреть возможно изменения текста в каждом элементе списка.
+    Добавить возможность перемещения элементов в списке.
+    Создать кнопку для добавления нового языка программирования в список.
+    Иконку для него поставить любую (по-умолчанию).
+    Создать кнопку для удаления элемента из списка (в этом случае элемент должен быть выбран).
+    Создать CheckBox и когда будет стоять галочка, отобразить список в режиме иконок (нужен метод setViewMode()).
+    Когда галочка не стоит, то список должен отображаться в обычном режиме.
+    Подсказка: используйте справку qt, вызов F1.
+*/
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    strModel  = new QStringListModel    (this);
+
+    itemModel = new QStandardItemModel  (this);
+
+    QStringList list;
+
+
+    list << "Строка 1" << "Строка 2" << "Строка 3" << "Строка 4";
+
+    strModel->setStringList(list);
+
+    ui->listView->setModel(strModel);
 }
 
 MainWindow::~MainWindow()
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
+#include <QStandardItemModel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QStringListModel *strModel;
+    QStandardItemModel *itemModel;
 };
 #endif // MAINWINDOW_H
