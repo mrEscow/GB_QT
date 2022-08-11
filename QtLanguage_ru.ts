38b13543c5a14499641d4a20e4cb7711134f384c
mr.Escow <escowxbox@gmail.com>
2022-08-11 13:45:08 +0300

    added new language(en)

--- QtLanguage_ru.ts
+++ QtLanguage_ru.ts
@@ -2,26 +2,41 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>HelpWidget</name>
+    <message>
+        <location filename="HelpWidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="HelpWidget.ui" line="43"/>
+        <source>Справка</source>
+        <translation>Справка</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
         <location filename="MainWindow.ui" line="14"/>
         <source>MainWindow</source>
-        <translation type="unfinished"></translation>
+        <translation>Текстовый редактор</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="25"/>
         <source>Сохранить</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="32"/>
         <source>Открыть</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранить</translation>
     </message>
     <message>
         <location filename="MainWindow.ui" line="43"/>
+        <location filename="MainWindow.cpp" line="78"/>
+        <location filename="MainWindow.cpp" line="79"/>
         <source>Справка</source>
-        <translation type="unfinished"></translation>
+        <translation>Справка</translation>
     </message>
 </context>
 </TS>
