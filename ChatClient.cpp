e7b6f9fc31f9d206d0fea7e63317d37428d60275
mr.Escow <escowxbox@gmail.com>
2022-10-17 17:41:34 +0300

    Init commit

---
+++ ChatClient.cpp
@@ -0,0 +1,14 @@
+#include "ChatClient.h"
+
+const quint16 port = 55555;
+
+ChatClient::ChatClient(QWidget *parent)
+    : QWidget(parent)
+{
+
+}
+
+ChatClient::~ChatClient()
+{
+
+}
---
+++ ChatClient.h
@@ -0,0 +1,21 @@
+#ifndef CHATCLIENT_H
+#define CHATCLIENT_H
+
+#include <QWidget>
+class ChatClient : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ChatClient(QWidget *parent = nullptr);
+    ~ChatClient();
+
+
+private slots:
+
+
+private:
+
+};
+
+#endif // CHATCLIENT_H
